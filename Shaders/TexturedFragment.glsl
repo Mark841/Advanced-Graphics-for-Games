1299569f5d85e71ee0308c3dc3e6b34cf8bcf007
Mark841 <lumb.mark@yahoo.co.uk>
2021-11-07 22:37:22 +0000

    Tutorial 9

--- Shaders/TexturedFragment.glsl
+++ Shaders/TexturedFragment.glsl
@@ -5,12 +5,23 @@ uniform sampler2D diffuseTex;
 in Vertex
 {
 	vec2 texCoord;
-	smooth vec4 colour;
+	// 1 - smooth vec4 colour;
+	vec4 jointWeights;
 } IN;
 
 out vec4 fragColour;
 
 void main(void)
 {
-	fragColour = texture(diffuseTex, IN.texCoord) * IN.colour;
+	// 1 - fragColour = texture(diffuseTex, IN.texCoord) * IN.colour;
+	if(IN.jointWeights.yz == vec2(0,0)){
+        fragColour = texture(diffuseTex, IN.texCoord) * IN.jointWeights;
+    }
+	else {
+		fragColour = texture(diffuseTex, IN.texCoord) * vec4(0, 1, 0, 0);
+	}
+	//fragColour = texture(diffuseTex, IN.texCoord);
 }
+
+// The commented out "1 -" sections were used to make shading on the terrain mesh
+// The commented out "2 -" sections were used to make colouring on the skeletal animation
